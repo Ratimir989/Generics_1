71c7304b9ef21e20e5ac79c22e2f529a31a28abd
Ratimir <ra0953029364@gmail.com>
2020-04-01 00:46:36 +0300

    Добавьте файлы проекта.

---
+++ Generics_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generics_1
+{
+    class MyClass<T>
+    {
+        public static MyClass<T> FactoryMethod()
+        {
+            return new MyClass<T>();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value1 = MyClass<int>.FactoryMethod();
+            var value2 = MyClass<string>.FactoryMethod();
+
+            Console.ReadLine();
+        }
+    }
+}
